fd4065c65545f8d9755ad1d9c9202b4e1b0cce3c
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-07 12:44:45 +0300

    delta

---
+++ build/precache-manifest.91b407ddf208e034152348fa016f2e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2088d1fbebcf1e2206348037d00e824f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5977b978ed8450c2296",
+    "url": "/static/css/main.0f85b6f1.chunk.css"
+  },
+  {
+    "revision": "fafccb674c7339c979eb",
+    "url": "/static/js/2.a50443ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a50443ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5977b978ed8450c2296",
+    "url": "/static/js/main.ff2760b4.chunk.js"
+  },
+  {
+    "revision": "7b825c87b8ad56c44788",
+    "url": "/static/js/runtime-main.95100870.js"
+  }
+]);
